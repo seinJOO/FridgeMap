4a8ba68b09fc94dd9881d7178a0128534389a545
querer94 <95738480+querer94@users.noreply.github.com>
2022-03-12 13:18:56 +0900

    Add files via upload

---
+++ FridgeMap/src/main/java/com/zerock/user/service/UserService.java
@@ -0,0 +1,15 @@
+package com.zerock.user.service;
+
+import com.zerock.user.command.UserVO;
+
+public interface UserService {
+
+	public int checkId(String user_id); // 아이디 중복 확인
+	public int checkNick(String user_nick); // 닉네임 중복 확인
+	public int join(UserVO vo);  // 회원가입
+	public int login(UserVO vo); // 회원 로그인
+	public UserVO info(String user_id); // 회원정보 보기
+	public int updateInfo(UserVO vo); // 회원정보 수정
+	public int delete(String user_id); // 회원정보 삭제
+		
+}
